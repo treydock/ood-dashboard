586d2f74b082987e343b1e3f8ce62b2333442d92
Eric Franz <efranz@osc.edu>
2016-05-12 10:26:35 -0400

    add datatables

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-// require dataTables/jquery.dataTables
-// require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require_tree .
